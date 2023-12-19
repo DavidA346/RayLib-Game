ee387b12828c064caee5f312baab72d5691e1ab4
DavidA346 <davidaispuro346@gmail.com>
2023-12-18 16:12:26 -0800

    Fixing the folders and added .gitignore

--- build/CMakeFiles/gameApp.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/gameApp.dir/src/main.cpp.o: \
-  /Users/davidaispuro/rayLibGame/src/main.cpp \
-  /Users/davidaispuro/rayLibGame/include/raylib.h \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/15.0.0/include/stdarg.h
